b9de4f43f6c5ab8b94ed10a1c6fcb66308175437
K.Asai <kra.enmn0618.swift@softbank.ne.jp>
2017-12-04 13:38:05 +0900

    Fix contact

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -1,10 +1,11 @@
+var isFetching = false;
+
 var postContact = () => {
   'use strict'
 
   var name        = document.getElementById('contact-name');
   var email       = document.getElementById('contact-email');
   var message     = document.getElementById('contact-message');
-  var subject     = name.value + ':email=' + email.value;
   var apigClient  = apigClientFactory.newClient();
 
   if (name.value === '') { return true; }
@@ -12,18 +13,23 @@ var postContact = () => {
   if (message.value === '') { return true; }
 
   var body = {
-    subject: subject,
-    message: message
+    subject: name.value + ':email=' + email.value,
+    message: message.value
   };
 
+  if (isFetching) { return false; }
   var promise = apigClient.contactsPost('', body)
+  isFetching = true;
+
   promise.then(response => {
     window.alert("お問い合わせを受け付けました。");
     window.location.reload();
+    isFetching = false;
   });
 
   promise.catch(response => {
     window.alert("Error: お問い合わせを送信できませんでした。");
+    isFetching = false;
   });
 
   return false;
